50aa8e4775b7da82c7ddda3c6af99bcadea1ee5c
cwc1222 <cwwalker088@gmail.com>
2025-05-02 01:39:41 -0300

    wip: Basice hcloud deployment

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # hcloud
 VPS management on Hetzner Cloud
+
+## How to find available images
+
+1. Use curl
+
+```bash
+curl \
+	-H "Authorization: Bearer $H_API_TOKEN" \
+	"https://api.hetzner.cloud/v1/images"
+
+curl \
+	-H "Authorization: Bearer $H_API_TOKEN" \
+	"https://api.hetzner.cloud/v1/images/$ID"
+```
+
+2. Use [hcloud cli](https://github.com/hetznercloud/cli)
+
+```bash
+hcloud context create <ur-project-name>
+hcloud image list -t system -a x86
+```
+
+## Reference
+
+- [developer.hashicorp.com/terraform/intro](https://developer.hashicorp.com/terraform/intro)
+- [docs.hetzner.com/cloud](https://docs.hetzner.com/cloud)
+- [registry.terraform.io/providers/hetznercloud/hcloud/latest/docs](https://registry.terraform.io/providers/hetznercloud/hcloud/latest/docs)
+- [developer.hashicorp.com/terraform/tutorials/provision/cloud-init](https://developer.hashicorp.com/terraform/tutorials/provision/cloud-init)
+- [dennmart.com/articles/get-started-with-hetzner-cloud-and-terraform-for-easy-deployments/](https://dennmart.com/articles/get-started-with-hetzner-cloud-and-terraform-for-easy-deployments/)
+- [medium.com/@orestovyevhen/set-up-infrastructure-in-hetzner-cloud-using-terraform-ce85491e92d](https://medium.com/@orestovyevhen/set-up-infrastructure-in-hetzner-cloud-using-terraform-ce85491e92d)
+- [community.hetzner.com/tutorials/setup-your-own-scalable-kubernetes-cluster](https://community.hetzner.com/tutorials/setup-your-own-scalable-kubernetes-cluster)
+- [scottspence.com/posts/setting-up-my-vps-on-hetzner](https://scottspence.com/posts/setting-up-my-vps-on-hetzner)
+- [github.com/hetznercloud/cli](https://github.com/hetznercloud/cli)
---
+++ main.tf
@@ -0,0 +1,186 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "1.50.1"
+    }
+  }
+}
+
+variable "hcloud_token" {
+  sensitive = true
+  type = string
+  description = "Hetzner Cloud API Token"
+}
+
+variable "ssh_pubkey" {
+  type = string
+  description = "SSH Public Key"
+}
+
+variable "ssh_user" {
+  type = string
+  description = "SSH User"
+}
+
+variable "server_name" {
+  type = string
+  description = "Server Name"
+}
+
+variable "server_type" {
+  # hcloud server-type list
+  type = string
+  default = "cx22"
+  description = "Server Type, default to be cx22 shared intel 2vCPU 4GB RAM"
+}
+
+variable "image" {
+  # https://docs.hetzner.cloud/#images
+  # https://www.reddit.com/r/hetzner/comments/12jrh42/terraform_with_hetzner/
+  # hcloud image list -t system -a x86
+  type = string
+  default = "debian-12"
+  description = "Image"
+}
+
+variable "location_zone" {
+  type = string
+  default = "eu-central"
+  description = "Location Zone"
+}
+
+variable "location" {
+  # hcloud location list
+  type = string
+  default = "fsn1"
+  description = "Location"
+}
+
+variable "datacenter" {
+  # hcloud datacenter list
+  type = string
+  default = "fsn1-dc14"
+  description = "Datacenter"
+}
+
+variable "labels" {
+  type = map(string)
+  description = "Labels"
+}
+
+variable "firewall_name" {
+  type = string
+  description = "Firewall Name"
+}
+
+variable "network_name" {
+  type = string
+  description = "Network Name"
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_firewall" "firewall" {
+  name = var.firewall_name
+
+  rule {
+    description = "Allow SSH traffic"
+    direction = "in"
+    protocol = "tcp"
+    port = "22"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    description = "Allow HTTP traffic"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "80"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    description = "Allow HTTPS traffic"
+    direction   = "in"
+    protocol    = "tcp"
+    port        = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
+
+resource "hcloud_network" "network" {
+  name = var.network_name
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_network_subnet" "subnet" {
+  network_id = hcloud_network.network.id
+  type = "cloud"
+  network_zone = var.location_zone
+  ip_range = "10.0.1.0/24"
+}
+
+resource "hcloud_primary_ip" "primary_ip" {
+  name = "chenantunez.com"
+  datacenter = var.datacenter
+  type = "ipv4"
+  assignee_type = "server"
+  auto_delete = true
+}
+
+resource "hcloud_server" "server" {
+  name = var.server_name
+  image = var.image
+  server_type = var.server_type
+  location = var.location
+  datacenter = var.datacenter
+  firewall_ids = [hcloud_firewall.firewall.id]
+  user_data = templatefile("${path.module}/cloud-init.yaml", {
+    ssh_pubkey = var.ssh_pubkey
+    ssh_user = var.ssh_user
+  })
+
+  labels = var.labels
+
+  public_net {
+    ipv4_enabled = true
+    ipv4 = hcloud_primary_ip.primary_ip.id
+    ipv6_enabled = true
+  }
+
+  network {
+    network_id = hcloud_network.network.id
+    ip = "10.0.1.1"
+  }
+
+  depends_on = [ hcloud_network_subnet.subnet ]
+}
+
+output "server_location" {
+  description = "Server Location and Zone"
+  value = "zone: ${var.location_zone}, location: ${var.location}, datacenter: ${var.datacenter}"
+}
+
+output "server_ipv4" {
+  description = "Server IPv4"
+  value = hcloud_server.server.ipv4
+}
+
+output "server_ipv6" {
+  description = "Server IPv6"
+  value = hcloud_server.server.ipv6
+}
+
+output "server_ssh_user" {
+  description = "Server SSH User"
+  value = var.ssh_user
+}
